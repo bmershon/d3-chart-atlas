f5f2f9aece89cb2a1b551a881a14d6bb4709f7d4
Brooks Mershon <mershon.brooks@gmail.com>
2015-07-31 11:00:20 -0400

    new interactions example added

--- examples/interactions/app.js
+++ examples/interactions/app.js
@@ -3,19 +3,6 @@
   var width = 960,
       height = 500;
 
-  var λ = d3.scale.linear()
-      .domain([0, width])
-      .range([-180, 180]);
-
-  var φ = d3.scale.linear()
-      .domain([0, height])
-      .range([90, -90]);
-
-  var time0 = Date.now(),
-      time1;
-
-  var timer = d3.select("#timer span");
-
   var data;
 
   var svg = d3.select("#map")
@@ -29,20 +16,24 @@
   options.layers.push({
     class: function(d, i) {return "country " + d.properties.continent.replace(/\s+/g, '') + " " + d.properties.admin.replace(/\s+/g, '')},
     object: "countries",
-    id: function(d, i) {return d.properties.admin.replace(/\s+/g, '');}
+    id: function(d, i) {return d.properties.admin.replace(/\s+/g, '');},
+    interactions: {
+      "mouseenter": function(d, i) {d3.select(this).classed("active", true)},
+      "mouseleave": function(d, i) {d3.select(this).classed("active", false)}
+    }
   });
 
   options.layers.push({
     class: function(d, i) {return "test " + d.properties.country},
     object: "nuclear",
-    id: function(d, i) {return "test-" + d.properties.year},
-    filter: function(d, i) {return d.properties.year <= 2005}
+    id: function(d, i) {return "test-" + d.properties.year}
   });
 
   // optional projection, orthographic is the default
   var m = svg.chart("atlas", options)
-             .graticule(d3.geo.graticule().step([20, 20]))
-             .projection(d3.geo.orthographic().clipAngle(90 + 10e-6))
+             .graticule(d3.geo.graticule().outline)
+             .rotate([-140, 0])
+             .projection(d3.geo.robinson())
              .pointRadius(4);
 
   queue()
@@ -54,16 +45,6 @@
     data = topology;
     m.draw(data)
      .zoomToLayer("countries");
-
-    svg.on("mousemove", function() {
-      var p = d3.mouse(this);
-      time0 = Date.now();
-
-      m.rotate([λ(p[0]), φ(p[1])]);
-
-      time1 = Date.now();
-      timer.text((time1 - time0));
-    });
 
   }
 
